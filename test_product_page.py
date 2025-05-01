47b740496ef2810a4a5508817f904e12bcc5db34
education <akornilov1999@gmail.com>
2025-05-01 05:05:29 +0300

    Change file test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -30,19 +30,23 @@ class TestGuestAddToBasketFromProductPage():
 		page = ProductPage(browser, link)
 		page.open()
 		page.go_to_login_page()
+		page = LoginPage(browser, browser.current_url)
+		page.should_be_login_page()
 
 	@pytest.mark.need_review
-	def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
-		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
-		page = ProductPage(browser, link)
-		page.open()
-		page.add_product_to_basket()
+	@pytest.mark.parametrize('should_basket_be_empty', [True, False])
+	def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser, should_basket_be_empty):
+		if not should_basket_be_empty:
+			link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+			page = ProductPage(browser, link)
+			page.open()
+			page.add_product_to_basket()
 		link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 		page = ProductPage(browser, link)
 		page.open()
 		page.go_to_basket_page()
 		page = BasketPage(browser, browser.current_url)
-		page.should_not_be_empty_basket()
+		page.should_be_empty_basket() if should_basket_be_empty else page.should_not_be_empty_basket()
 
 @pytest.mark.negative_basket
 class TestNegativeBasket():
